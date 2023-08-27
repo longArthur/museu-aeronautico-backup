bab19003859202ab2fa75371a73a25d5c77bb969
arthurfs <ferreirinhaar@gmail.com>
2023-08-27 10:08:42 -0300

    O que o caio fez mas é banzo demais pra colocar no GitHub

--- src/Interfaces/Base.java
+++ src/Interfaces/Base.java
@@ -4,4 +4,8 @@ import javax.swing.*;
 
 public class Base {
     private JPanel panel1;
+
+    private void createUIComponents() {
+        // TODO: place custom component creation code here
+    }
 }
